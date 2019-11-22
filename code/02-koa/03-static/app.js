856843fe008ded1c9e956fbfd046d41c130353ac
jj280522 <jj280522@gmail.com>
2019-11-22 09:20:10 +0800

    ADD1

--- code/02-koa/03-static/app.js
+++ code/02-koa/03-static/app.js
@@ -1,9 +1,9 @@
-const serve = require('koa-static');
-const Koa = require('koa');
-const app = new Koa();
+const serve = require('koa-static'); //koa靜態服務
+const Koa = require('koa'); //引用koa框架
+const app = new Koa();  //建立Koa物件
 
-app.use(serve(__dirname + '/public'));
+app.use(serve(__dirname + '/public')); //設置靜態檔案服務 (__dirname為node.js關鍵詞)表實體目錄
 
-app.listen(3000);
+app.listen(3000); //聽3000埠
 
-console.log('listening on port 3000');
+console.log('listening on port 3000'); //紀錄聽3000埠
